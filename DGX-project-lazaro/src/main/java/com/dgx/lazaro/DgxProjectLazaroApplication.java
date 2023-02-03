36d0394e46a5b32a317eedf04127a4621e5f09ac
lazaro.silva <lazaro.silva@datainfo.inf.br>
2023-02-03 20:20:27 -0300

    main adicionando application

---
+++ DGX-project-lazaro/src/main/java/com/dgx/lazaro/DgxProjectLazaroApplication.java
@@ -0,0 +1,14 @@
+package com.dgx.lazaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DgxProjectLazaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgxProjectLazaroApplication.class, args);
+	}
+
+}

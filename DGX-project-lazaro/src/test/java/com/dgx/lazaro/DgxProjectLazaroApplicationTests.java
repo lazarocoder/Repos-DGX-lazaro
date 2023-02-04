560cc3aa1a42341833292f055100deab5c8701f4
lazaro.silva <lazaro.silva@datainfo.inf.br>
2023-02-03 21:15:07 -0300

    main adicionando classes

---
+++ DGX-project-lazaro/src/test/java/com/dgx/lazaro/DgxProjectLazaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgx.lazaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgxProjectLazaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

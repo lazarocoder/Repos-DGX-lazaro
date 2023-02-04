e6e725da3f8f7bfdbf9183fffb93061bc7e49584
lazaro.silva <lazaro.silva@datainfo.inf.br>
2023-02-04 18:35:23 -0300

    main adicionando teste na controller

--- DGX-project-lazaro/src/test/java/com/dgx/lazaro/controllerTeste/FamiliaControllerTeste.java
+++ DGX-project-lazaro/src/test/java/com/dgx/lazaro/controllerTeste/FamiliaControllerTeste.java
@@ -12,10 +12,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;

3439adc15964a2df6a7e0977ff276c6070b9b650
lazaro.silva <lazaro.silva@datainfo.inf.br>
2023-02-04 18:28:01 -0300

    main adicionando dependencias de teste e teste na controller

---
+++ DGX-project-lazaro/src/test/java/com/dgx/lazaro/controllerTeste/FamiliaControllerTeste.java
@@ -0,0 +1,72 @@
+package com.dgx.lazaro.controllerTeste;
+
+import com.dgx.lazaro.controller.FamiliaController;
+import com.dgx.lazaro.dto.FamiliaDTO;
+import com.dgx.lazaro.service.FamiliaService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FamiliaControllerTeste {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private FamiliaService familiaService;
+
+    @InjectMocks
+    private FamiliaController familiaController;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(familiaController)
+                .build();
+    }
+
+    @Test
+    public void testConsultarRank_WhenListIsNotEmpty_ShouldReturn200() throws Exception {
+        List<FamiliaDTO> familiaDTOList = new ArrayList<>();
+        familiaDTOList.add(new FamiliaDTO());
+
+        when(familiaService.getListaOrdenada()).thenReturn(familiaDTOList);
+
+        mockMvc.perform(get("/familia/rank")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testConsultarRank_WhenListIsEmpty_ShouldReturn204() throws Exception {
+        List<FamiliaDTO> familiaDTOList = new ArrayList<>();
+
+        when(familiaService.getListaOrdenada()).thenReturn(familiaDTOList);
+
+        mockMvc.perform(get("/familia/rank")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testConsultarRank_WhenExceptionIsThrown_ShouldReturn500() throws Exception {
+        when(familiaService.getListaOrdenada()).thenThrow(new Exception());
+
+        mockMvc.perform(get("/familia/rank")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+    }
+}
+
